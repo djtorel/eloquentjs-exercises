2ae8be05d21cc806f10771f142abe7c807a0a2e1
Dominic Torruellas <dom@domtorr.com>
2019-01-18 14:16:55 -0700

    Ch02-Ex02-FizBuzz - Completed

---
+++ 02-Program-Structure/Exercises/02-FizBuzz/fizbuzz.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 100; i++) {
+  let output = '';
+  if (!(i % 3)) {
+    output += "Fizz";
+  }
+  if (!(i % 5)) {
+    output += "Buzz";
+  }
+  if (output.length === 0) {
+    output = i;
+  }
+
+  console.log(output);
+}

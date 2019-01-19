5e6dbc40862412c549fb9fbd82e3b67389b3ab21
Dominic Torruellas <dom@domtorr.com>
2019-01-18 17:17:01 -0700

    Removed un-needed if statement - set conditional for output

--- 02-Program-Structure/Exercises/02-FizBuzz/fizbuzz.js
+++ 02-Program-Structure/Exercises/02-FizBuzz/fizbuzz.js
@@ -6,9 +6,6 @@ for (let i = 1; i <= 100; i++) {
   if (!(i % 5)) {
     output += "Buzz";
   }
-  if (output.length === 0) {
-    output = i;
-  }
 
-  console.log(output);
+  console.log(output || i);
 }

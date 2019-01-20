1e5bf9f5cd70d84e03bc3c27c4f200f4cacd2110
Dominic Torruellas <dom@domtorr.com>
2019-01-19 18:28:34 -0700

    Ch02-Ex02-FizzBuzz - Played around with making it expandable

--- 02-Program-Structure/Exercises/02-FizBuzz/fizbuzz.js
+++ 02-Program-Structure/Exercises/02-FizBuzz/fizbuzz.js
@@ -1,3 +1,4 @@
+/* 
 for (let i = 1; i <= 100; i++) {
   let output = "";
   if (!(i % 3)) {
@@ -6,6 +7,25 @@ for (let i = 1; i <= 100; i++) {
   if (!(i % 5)) {
     output += "Buzz";
   }
+
+  console.log(output || i);
+}
+ */
+
+const fizzDictionary = [
+  { 3: "Fizz" },
+  { 5: "Buzz" },
+];
+
+for (let i = 1; i <= 100; i++) {
+  const output = fizzDictionary.reduce((acc, item) => {
+    for (let key in item) {
+      if (!(i % key)) {
+        acc += item[key];
+      }
+      return acc;
+    }
+  }, "");
 
   console.log(output || i);
 }

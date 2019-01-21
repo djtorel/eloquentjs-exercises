d9f12a1aeff062c99ca2a5dff5430503598eea71
Dominic Torruellas <dom@domtorr.com>
2019-01-21 00:39:19 -0700

    Ch04-Ex02-Reversing-An-Array - Completed

--- 04-Data/Exercises/02-Reversing-An-Array/reversing-an-array.js
+++ 04-Data/Exercises/02-Reversing-An-Array/reversing-an-array.js
@@ -6,3 +6,27 @@ reverseArrayInPlace, does what the reverse method does: it modifies the array
 given as argument by reversing its elements. Neither may use the standard 
 reverse method.
 */
+
+const reverseArray = (arr) => {
+  return arr.reduce((acc, value) => {
+    acc.unshift(value);
+    return acc;
+  }, []);
+};
+
+const reverseArrayInPlace = (arr) => {
+  let iterLength = Math.floor(arr.length / 2);
+
+  for (let i = 0; i < iterLength; i++) {
+    const frontSwap = arr[arr.length - 1 - i];
+    const endSwap = arr[i];
+    arr[i] = frontSwap;
+    arr[arr.length - 1 - i] = endSwap;
+  }
+};
+
+console.log(reverseArray([1, 2, 3]));
+let testArr = [4, 5, 6, 7, 8];
+console.log(`testarr: [${testArr}]`);
+reverseArrayInPlace(testArr);
+console.log(`testarr: [${testArr}]`);
